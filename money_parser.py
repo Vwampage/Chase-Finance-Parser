cfe1153ebcaee70b0df23c74fc2c1e1251259b89
David Michaels <dmichaels@signal.co>
2017-01-16 18:24:22 -0600

    Added support for reading in a manually created list of categories and splitting up money by that quantity. Needs work as the totals do not yet match.

--- money_parser.py
+++ money_parser.py
@@ -26,21 +26,66 @@ def generate_transaction_list(unparsed_transactions):
 		all_transactions.append(transaction_object)
 	return all_transactions
 
+def parsing_categories(unparsed_categories):
+	categories = {}
+	for i in unparsed_categories:
+		split_categories = i.split(',')
+		if 'Category' in split_categories[1]:
+			continue
+		categories[split_categories[0].strip()] = split_categories[1].strip()
+	return categories
+
+
+def create_unique_category_list():
+	list_of_categories = {}
+	for i in category_dict:
+		one_category = category_dict[i]
+		if one_category not in list_of_categories:
+			list_of_categories[one_category] = 0.0
+	return list_of_categories
+
+
+
+
+
 def monthly_total(year, month, transaction_type):
 	month_total = 0.0
+	category_totals = create_unique_category_list()
 	for i in parsed_transactions:
 		if i['transaction_date']['year'] == year:
 			if i['transaction_date']['month'] == month:
 				if transaction_type in i['type']:
 					month_total += i['amount']
-	return month_total
+					for j in category_dict:
+						if j in i['description']:
+							category_totals[category_dict[j]] += i['amount']
+	return (month_total, category_totals)
+
 
 
 raw_transactions = open_transactions_csv('/Users/vwampage/dev/chase_data/november2016_january2017.csv')
 
 parsed_transactions = generate_transaction_list(raw_transactions)
 
-print monthly_total(2016, 10, 'Sale')
+category_dict = parsing_categories(open_transactions_csv('/Users/vwampage/dev/chase_data/categories3.csv'))
+
+category_list = create_unique_category_list()
+
+
+
+totals_tuple = monthly_total(2016, 11, 'Sale')
+
+print totals_tuple[0]
+print totals_tuple[1]
+check_category_totals = 0.0
+for i in totals_tuple[1]:
+	check_category_totals += totals_tuple[1][i]
+print check_category_totals
+
+
+
+
+# print category_dict
 
 # for i in raw_transactions:
 # 	length_of_line = len(i.split(','))
